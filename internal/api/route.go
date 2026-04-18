1c79f4a1497b59bd11ac6c171e22d6704e7157ac
John Heng <john@gosoft.co.th>
2026-04-19 02:54:43 +0700

    refactor: improve logging message in Start function
    Signed-off-by: John Heng <john@gosoft.co.th>

--- internal/api/route.go
+++ internal/api/route.go
@@ -50,7 +50,7 @@ import (
 )
 
 func Start(config *config.GlobalConfig, port int, proPort int) error {
-	log.ZDebug(context.Background(), "configAPI1111111111111111111", config, "port", port, "javafdasfs")
+	log.ZDebug(context.Background(), "api starting", "config", config, "port", port)
 	if port == 0 || proPort == 0 {
 		err := "port or proPort is empty:" + strconv.Itoa(port) + "," + strconv.Itoa(proPort)
 		return errs.Wrap(fmt.Errorf(err))
